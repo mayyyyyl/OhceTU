96efdbc9734ce4b6ffbe8fbee368496e02009185
Maylis Le Borgne <maylislb@hotmail.fr>
2023-01-30 18:11:46 +0100

    test_miroir ok

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ class Ohce:
     def chaine_miroir(self, mot):
         """ Renvoie la chaine en miroir """
         
+        chaine_miroir = mot[::-1]
+
+        chaine_miroir = mot[::1]
+
+        if mot == chaine_miroir:
+            return f"Bonjour {chaine_miroir}  \n Bien dit"
+
+        return f"Bonjour {chaine_miroir} \n Au revoir"
