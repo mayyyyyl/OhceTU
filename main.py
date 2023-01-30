f83751d30367591e2df238b600a21cea7bd3342b
Maylis Le Borgne <maylislb@hotmail.fr>
2023-01-30 18:41:39 +0100

    test_bonjour et test_aurevoir avec la langue ok

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 class Ohce:
-    def chaine_miroir(self, mot):
-        """ Renvoie la chaine en miroir """
+    def chaine_miroir(self, langue, mot):
+        """ Renvoie la chaine en miroir en fonction de la langue """
         
         chaine_miroir = mot[::-1]
 
-        if mot == chaine_miroir:
-            return f"Bonjour {chaine_miroir}  \nBien dit"
+        if langue == 'fr':
+            if mot == chaine_miroir :
+                return f"Bonjour \n{chaine_miroir}  \nBien dit"
+            else:
+                return f"Bonjour \n{chaine_miroir} \nAu revoir"
 
-        return f"Bonjour {chaine_miroir} \nAu revoir"
+        if langue == "en":
+            if mot == chaine_miroir :
+                return f"Hello \n{chaine_miroir}  \nWell said"
+            else:
+                return f"Hello \n{chaine_miroir} \nGood Bye"
